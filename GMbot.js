7d3721d1f7c2c7fd07b97ad6f0db9e4a359445cc
Jakub Robaczewski <kubarob13@gmail.com>
2022-06-14 16:24:18 +0200

    Cleaning

--- GMbot.js
+++ GMbot.js
@@ -1,57 +1,50 @@
-const { Client, intents, MessageEmbed, Message, MessageAttachment,  Permissions } = require('discord.js');
-var fs = require('fs');
-const { readFile } = require('fs/promises');
-const Canvas = require('@napi-rs/canvas');
-const { request } = require('undici');
+const {Client} = require('discord.js');
+const fs = require('fs');
 const client = new Client({
-  intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"],
-  partials: ["USER", "REACTION", "MESSAGE", "GUILD_SCHEDULED_EVENT", "GUILD_MEMBER", "CHANNEL"]
+    intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_BANS", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_WEBHOOKS", "GUILD_INVITES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILD_MESSAGE_TYPING", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGE_TYPING"],
+    partials: ["USER", "REACTION", "MESSAGE", "GUILD_SCHEDULED_EVENT", "GUILD_MEMBER", "CHANNEL"]
 })
 const data = require("./data");
-const GMrank = require('./GMrank.json');
-
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setStatus(`online`);
-});
+const GM_ranks = require('./GMrank.json');
 
 client.on("message", newRank => {
-  if (newRank.content.toLowerCase().startsWith(data.prefix + "rank")) {
-    console.log("Komenda wykryta");
+    if (newRank.content.toLowerCase().startsWith(data.prefix + "rank")) {
+        console.log("Komenda wykryta");
 
-    //Zabezpieczenia
-    if (newRank.member == null) return newRank.reply("Komendy można użyć tylko na serwerze")
-    if (newRank.mentions.users.first() == null) return newRank.reply("Nie wybrano użytkownika")
+        //Zabezpieczenia
+        if (newRank.member == null) return newRank.reply("Komendy można użyć tylko na serwerze")
+        if (newRank.mentions.users.first() == null) return newRank.reply("Nie wybrano użytkownika")
 
-    GMrank['GMS'].forEach(GMID => {
-      if (newRank.author.id == GMID["GMID"]) {
-        console.log("osoba jest mistrzem gry");
+        GM_ranks['GMS'].forEach(GMID => {
+            if (newRank.author.id === GMID["GMID"]) {
+                console.log("osoba jest mistrzem gry");
 
-        if (newRank.guild.roles.cache.find(r => r.id === GMID["RankID"]) == null) return newRank.reply("Ranga nie istnieje")
-        let role = newRank.guild.roles.cache.find(r => r.id === GMID["RankID"]);
-        //Wyszukujemy roli korzystając z RankID
-        let member = newRank.mentions.members.first();
-        member.roles.add(role);
-  }})}
+                if (newRank.guild.roles.cache.find(r => r.id === GMID["RankID"]) == null) return newRank.reply("Ranga nie istnieje")
+                let role = newRank.guild.roles.cache.find(r => r.id === GMID["RankID"]);
+                //Wyszukujemy roli korzystając z RankID
+                let member = newRank.mentions.members.first();
+                member.roles.add(role).then(() => console.log("Role added"));
+            }
+        })
+    }
 })
 
 client.on("message", async set => {
-  if (set.content.toLowerCase().startsWith(data.prefix + "set")) {
-    console.log("Komenda wykryta");
+    if (set.content.toLowerCase().startsWith(data.prefix + "set")) {
+        console.log("Komenda wykryta");
 
-    //Zabezpieczenia
-    if (set.member == null) return set.reply("Komendy można użyć tylko na serwerze")
-    if (set.member.permissions.has("ADMINISTRATOR") != true) return set.reply("Ta komenda wymaga uprawnień administratora")
-    if (set.mentions.users.first() == null) return set.reply("Brak ustawionego GMa")
-    if (set.mentions.roles.first() == null) return set.reply("Brak ustawionej rangi")
+        //Zabezpieczenia
+        if (set.member == null) return set.reply("Komendy można użyć tylko na serwerze")
+        if (set.member.permissions.has("ADMINISTRATOR") !== true) return set.reply("Ta komenda wymaga uprawnień administratora")
+        if (set.mentions.users.first() == null) return set.reply("Brak ustawionego GMa")
+        if (set.mentions.roles.first() == null) return set.reply("Brak ustawionej rangi")
 
-    GMrank['GMS'].push({ 
-      GMID: set.mentions.users.first().id,
-      RankID: set.mentions.roles.first().id
-    });
-      fs.writeFile('./GMrank.json', JSON.stringify(GMrank), function(){});
-  }
+        GM_ranks['GMS'].push({
+            GM_id: set.mentions.users.first().id,
+            RankID: set.mentions.roles.first().id
+        });
+        fs.writeFile('./GMrank.json', JSON.stringify(GM_ranks), function () {});
+    }
 })
 
-
-client.login(data.token);
+client.login(data.token).then(() => console.log("Bot logged"));
--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token:``,
+    token:`Mzc4MjQ2MTAxNzU0NzA3OTcw.Ggrw-Q.XTFYB4w-vOu8kRhSju-2jL7qSLr6W7JUuskpNs`,
     prefix:`>`,
 }
